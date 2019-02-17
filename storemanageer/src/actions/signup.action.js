9e5e59d6c280f7de24090d3bb7c82c8beabddc2e
Allan Mogusu <allannelly690@gmail.com>
2019-02-17 20:39:42 +0300

    Sign up page

--- storemanageer/src/actions/signup.action.js
+++ storemanageer/src/actions/signup.action.js
@@ -1,6 +1,41 @@
-import {SEND_EMAIL, SIGNUP_ERRORS } from "./signupActionTypes";
-import axios from 'axios'
+import {SIGNUP_ERRORS, SIGNUP_SUCCESSFUL} from "./signupActionTypes";
+import axios from 'axios';
+import {toast} from 'react-toastify'
 
-export const sendEmail = email => dispatch => {
-    axios.post()
+export const signUpErrors = errors => {
+    return {
+        type: SIGNUP_ERRORS,
+        payload: errors
+    }
+};
+
+export const signUpUser = data => dispatch =>{
+      if (!localStorage.getItem('token')){
+          toast.error('Please login with your admin credentials to proceed!');
+      }
+      const token = localStorage.getItem('token');
+      axios.post('https://store-manager-api-app-v2.herokuapp.com/api/v2/auth/signup', data, {
+          headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin':'*',
+          'Authorization': `Bearer ` + token
+      }
+      }
+
+      )
+          .then(response => {
+              dispatch({
+                  type: SIGNUP_SUCCESSFUL,
+                  payload: data.email
+              });
+              toast.success('User registered successfully!');
+          })
+          .catch(error =>{
+              console.log(error);
+              dispatch({
+                  type: SIGNUP_ERRORS,
+                  payload: error
+              });
+              toast.error('Something went awry. Please check your signup credentials');
+          })
 };
